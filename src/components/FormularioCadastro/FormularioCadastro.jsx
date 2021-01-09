6a33726b2fdeadbf941b21c4645e543ccc663677
Luís Fernando da Silveira <luisfernandosilveira23@gmail.com>
2021-01-09 20:31:54 -0300

    Início Material UI

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from "@material-ui/core/Button"
 
 // Criamos um function component
 // Trata-se de uma função que retorna um componente em jsx
@@ -6,7 +7,36 @@ import React from 'react';
 function FormularioCadastro() {
     return (
         <form action="">
-            <input type="text" />
+            <label htmlFor="">Nome</label>
+            <input
+                type="text"
+                placeholder="Ex: João"
+            />
+
+            <label htmlFor="">Sobrenome</label>
+            <input
+                type="text"
+                placeholder="Ex: Silva"
+            />
+
+            <label htmlFor="">CPF</label>
+            <input
+                type="text"
+                placeholder="Ex: 11122233300"
+            />
+
+            <label htmlFor="">Promoções</label>
+            <input type="checkbox" />
+
+            <label htmlFor="">Novidades</label>
+            <input type="checkbox" />
+
+            <Button
+                type="submit"
+                variant="contained"
+                color="primary">
+                Cadastrar
+            </Button>
         </form>
     )
 }
