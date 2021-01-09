0048130d078cb18a788407d31e7009948c16e228
Luís Fernando da Silveira <luisfernandosilveira23@gmail.com>
2021-01-09 20:03:39 -0300

    Início do projeto

---
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// Criamos um function component
+// Trata-se de uma função que retorna um componente em jsx
+// Geralmente function components são stateless components
+function FormularioCadastro() {
+    return (
+        <form action="">
+            <input type="text" />
+        </form>
+    )
+}
+
+export default FormularioCadastro;
