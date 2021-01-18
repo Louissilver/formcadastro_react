3b96fb02ddd135818fd2ac0750b342136f10bc66
Luís Fernando da Silveira <luisfernandosilveira23@gmail.com>
2021-01-18 20:56:17 -0300

    Mudança de condicional para array

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -10,27 +10,17 @@ import DadosUsuario from "./DadosUsuario";
 function FormularioCadastro({ aoEnviar, validarCPF }) {
   const [etapaAtual, setEtapaAtual] = useState(0);
 
-  function proximo(){
-    setEtapaAtual(etapaAtual+1);
+  const formularios = [
+    <DadosUsuario aoEnviar={proximo} />,
+    <DadosPessoais aoEnviar={proximo} validarCPF={validarCPF} />,
+    <DadosEntrega aoEnviar={aoEnviar} />,
+  ];
+
+  function proximo() {
+    setEtapaAtual(etapaAtual + 1);
   }
 
-  function formularioAtual(etapa) {
-    switch (etapa) {
-      case 0:
-        return <DadosUsuario aoEnviar={proximo} />;
-        break;
-      case 1:
-        return <DadosPessoais aoEnviar={proximo} validarCPF={validarCPF} />;
-        break;
-      case 2:
-        return <DadosEntrega aoEnviar={aoEnviar} />;
-        break;
-      default:
-        return <Typography>Erro</Typography>;
-    }
-  }
-
-  return <>{formularioAtual(etapaAtual)}</>;
+  return <>{formularios[etapaAtual]}</>;
 }
 
 export default FormularioCadastro;
