2606eb842e329baccc7970ad4b574edeb443d074
Luís Fernando da Silveira <luisfernandosilveira23@gmail.com>
2021-01-18 21:14:30 -0300

    Novos estados para os campos

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -16,7 +16,7 @@ function FormularioCadastro({ aoEnviar, validarCPF }) {
     <DadosEntrega aoEnviar={aoEnviar} />,
   ];
 
-  function proximo() {
+  function proximo(dados) {
     setEtapaAtual(etapaAtual + 1);
   }
 
